ca39a7d9c2f57f9ffd0519b0d2f458bde6aaa90b
ey1031 <eyudeveloper@gmail.com>
2017-11-01 16:14:18 -0700

    adding retrieving song title

--- src/animelyrics.py
+++ src/animelyrics.py
@@ -7,44 +7,44 @@ from urllib.parse import urlparse
 class AnimeLyrics:
 
 	# SEARCH_TERM: SONG TITLE
+	# PAGE_URL: animelyrics.com PAGE URL
+	# SOUP: HTML CONTENT FOR PAGE_URL
 
 	def __init__(self, keyword):
 		self.SEARCH_TERM = keyword
-
-	def lyrics(self):
-		resultArray = []
 		for url in search(self.SEARCH_TERM, tld='com.pk', lang='es', stop=10):
 			if (urlparse(url).hostname == "www.animelyrics.com"):
-				resultArray.append(url)
-				print(url)
+				self.PAGE_URL = url
 
-		url = resultArray[0]
-		html = requests.get(url)
-		soup = BeautifulSoup(html.text, 'html5lib')
+		html = requests.get(self.PAGE_URL)
+		self.SOUP = BeautifulSoup(html.text, 'html5lib')
 
-		tdSearch = soup.findAll("td", {
+	def lyrics(self):
+		tdSearch = self.SOUP.findAll("td", {
 			"class" : "translation"
-			});
+		});
+
 		count = 0
 		for element in tdSearch:
 			count = count + 1
 
 		lyrics = []
+
+		# --------------------- TABLES WITHOUT TRANSLATIONS ---------------------
 		if count == 0:
-			# --------------------- TABLES WITHOUT TRANSLATIONS ---------------------
-			result = self.find_between(soup.text, "Lyrics from Animelyrics.com", "Transliterated")
+			result = self.find_between(self.SOUP.text, "Lyrics from Animelyrics.com", "Transliterated")
 			lyrics.append(result.replace("\xa0", " ").strip())
 
+		# --------------------- TABLES WITH TRANSLATIONS ------------------------
 		elif count == 2:
-			# --------------------- TABLES WITH TRANSLATIONS ---------------------
-			for linebreak in soup.find_all('br'):
+			for linebreak in self.SOUP.find_all('br'):
 			    linebreak.extract()
-			for line in soup.find_all('dt'):
+			for line in self.SOUP.find_all('dt'):
 			    line.extract()
 
-			mydivs = soup.findAll("td", {
+			mydivs = self.SOUP.findAll("td", {
 				"class" : "romaji"
-				});
+			});
 
 			for x in mydivs:
 				lyrics.append(x.text.replace("\xa0", " ").strip())
@@ -55,9 +55,13 @@ class AnimeLyrics:
 		return ", ".join(lyrics)
 
 	def find_between(self, s, first, last):
-	    try:
-	        start = s.index( first ) + len( first )
-	        end = s.index( last, start )
-	        return s[start:end]
-	    except ValueError:
-	        return ""
+		try:
+			start = s.index( first ) + len( first )
+			end = s.index( last, start )
+			return s[start:end]
+		except ValueError:
+			return "Error in Page Structure"
+
+	def song_title(self):
+		return self.SOUP.findAll("h1")[1].get_text()
+
--- src/animelyrics_tests.py
+++ src/animelyrics_tests.py
@@ -1,4 +1,11 @@
 from animelyrics import AnimeLyrics
 
 al = AnimeLyrics("caste room")
+
+print("------------------------------------------")
+print("LYRICS:")
 print(al.lyrics())
+print("------------------------------------------")
+print("TITLE:")
+print(al.song_title())
+print("------------------------------------------")
